9f7aae0f0502ab54b8de3185027c54d08944bfa0
Ian Berg <53795111+ianberg-volpe@users.noreply.github.com>
2020-10-09 11:55:22 -0400

    update spec to follow json-schema

---
+++ update_for_json-schema.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 10:35:07 2020
+
+@author: ian.berg
+"""
+
+import os, json
+rt = "C:\\Users\\ian.berg\\Documents\\GitHub\\GMNSpy\\spec"
+for root, dir, files in os.walk(rt):
+    for file in files:
+        print(file)
+        if file == "gmns.spec.json":
+            continue
+        name = file[:file.find(".")]
+        f = open(os.path.join(rt, file))
+        table = json.load(f)
+        table["name"] = name
+        table["properties"] = {}    
+        for field in table['fields']:
+            field_name = field['name']
+            table['properties'][field_name] = field
+            del table['properties'][field_name]['name']
+            try:
+                table['properties'][field_name].update(table['properties'][field_name]['constraints'])
+                del table['properties'][field_name]['constraints']
+            except KeyError:
+                continue
+        del table["fields"]
+        f.close()
+        g = open(os.path.join(rt, file), "w")
+        json.dump(table, g, indent = 4)
+        g.close()
